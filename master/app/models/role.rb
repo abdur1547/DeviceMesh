cfff8c77c3861223a3a1f8b8722e5b51d2f77eea
Abdur Rahman <abdur@lycus.com>
2023-07-18 16:32:27 +0500

    role model is created.

---
+++ master/app/models/role.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: roles
+#
+#  id         :bigint           not null, primary key
+#  name       :string           default("normal"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_roles_on_name  (name) UNIQUE
+#
+class Role < ApplicationRecord
+  ADMIN = 'admin'
+  NORMAL = 'normal'
+  
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, inclusion: {in: [ADMIN, NORMAL]}
+end
