311467485bf03a3929ddc91990d816505642b435
Abdur Rahman <abdur@lycus.com>
2023-07-18 16:59:49 +0500

    DB is migrated.

--- master/db/schema.rb
+++ master/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_113319) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_115635) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_113319) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_roles_on_name", unique: true
+  end
+
+  create_table "slaves", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_slaves_on_name", unique: true
   end
 
   create_table "user_roles", force: :cascade do |t|
