ec427173e595bed466fbe0da32a01eff3227631f
Abdur Rahman <abdur@lycus.com>
2023-07-26 15:12:52 +0500

    lib & services are autoloaded.

--- master/config/application.rb
+++ master/config/application.rb
@@ -29,7 +29,8 @@ module Master
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("app/services")
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
