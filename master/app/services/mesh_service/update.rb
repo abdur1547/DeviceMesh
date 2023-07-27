7c29519d7fcffadd55f82f3e73fa8dd535ae645b
Abdur Rahman <abdur@lycus.com>
2023-07-27 23:36:31 +0500

    update mesh service is created.

---
+++ master/app/services/mesh_service/update.rb
@@ -0,0 +1,22 @@
+module MeshService
+  class Update < ApplicationService
+    attr_reader :id, :name, :current_user
+    
+    def initialize(id, name, current_user)
+      @id = id
+      @name = name
+      @current_user = current_user
+    end
+    
+    def call
+      mesh = Mesh.find_by(id: id)
+      return success(mesh) if mesh.update!(name: name)
+      rescue => e
+        return failure(e)
+    end
+  
+    def self.call(id:, name:, current_user:)
+      new(id, name, current_user).call
+    end
+  end
+end
