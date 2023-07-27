f4f1b282924601f773df8f9e0c4e54c93add14c6
Abdur Rahman <abdur@lycus.com>
2023-07-27 22:50:11 +0500

    Create mesh service is created.

--- master/app/services/mesh_service/create.rb
+++ master/app/services/mesh_service/create.rb
@@ -1,10 +1,21 @@
 module MeshService
-  class Create
-    include Interactor
-
+  class Create < ApplicationService
+    attr_reader :name, :current_user
+    
+    def initialize(name, current_user)
+      @name = name
+      @current_user = current_user
+    end
+    
     def call
-      puts "create a mesh"
-      puts "context #{context}"
+      mesh = Mesh.create!(name: name, user_id: current_user.id)
+      return success(mesh)
+      rescue => e
+        return failure(e)
+    end
+  
+    def self.call(name:, current_user:)
+      new(name, current_user).call
     end
   end
 end
