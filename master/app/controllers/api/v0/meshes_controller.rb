edd10e2c3765decd89ed861afc2cc0997279d377
Abdur Rahman <abdur@lycus.com>
2023-08-26 00:37:30 +0500

    converted to modules.

--- master/app/controllers/api/v0/meshes_controller.rb
+++ master/app/controllers/api/v0/meshes_controller.rb
@@ -1,36 +1,41 @@
-class Api::V0::MeshesController < ApiController
-  def index
-    result = MeshService::Index.call(current_user: current_user)
-    return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer,status: :ok ) if result[:success?]
-    return failure_json_response(errors: result[:errors], status: :not_found)
-  end
-
-  def create
-    result = MeshService::Create.call(name: mesh_params[:name], current_user: current_user)
-    return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer,status: :created ) if result[:success?]
-    return failure_json_response(errors: result[:errors], status: 403)
-  end
-
-  def show
-    result = MeshService::Show.call(id: params[:id], current_user: current_user)
-    return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer, status: :ok ) if result[:success?]
-    return failure_json_response(errors: result[:errors], status: :not_found)
-  end
-
-  def update
-    result = MeshService::Update.call(id: params[:id], name: params[:name], current_user: current_user)
-    return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer, status: :ok ) if result[:success?]
-    return failure_json_response(errors: result[:errors], status: :not_found)
-  end
-
-  def destroy
-    result = MeshService::Destroy.call(id: params[:id], current_user: current_user)
-    return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer, status: :ok ) if result[:success?]
-    return failure_json_response(errors: result[:errors], status: :not_found)
-  end
-
-  private
-  def mesh_params
-    params.permit(:name)
+module Api
+  module V0
+    class MeshesController < ApiController
+      def index
+        result = MeshService::Index.call(current_user: current_user)
+        return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer,status: :ok ) if result[:success?]
+        return failure_json_response(errors: result[:errors], status: :not_found)
+      end
+    
+      def create
+        result = MeshService::Create.call(name: mesh_params[:name], current_user: current_user)
+        return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer,status: :created ) if result[:success?]
+        return failure_json_response(errors: result[:errors], status: 403)
+      end
+    
+      def show
+        result = MeshService::Show.call(id: params[:id], current_user: current_user)
+        return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer, status: :ok ) if result[:success?]
+        return failure_json_response(errors: result[:errors], status: :not_found)
+      end
+    
+      def update
+        result = MeshService::Update.call(id: params[:id], name: params[:name], current_user: current_user)
+        return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer, status: :ok ) if result[:success?]
+        return failure_json_response(errors: result[:errors], status: :not_found)
+      end
+    
+      def destroy
+        result = MeshService::Destroy.call(id: params[:id], current_user: current_user)
+        return success_json_response(success: true, data: result[:data], serializer: Api::V0::MeshSerializer, status: :ok ) if result[:success?]
+        return failure_json_response(errors: result[:errors], status: :not_found)
+      end
+    
+      private
+      def mesh_params
+        params.permit(:name)
+      end
+    end
+    
   end
 end
