68e39bf09d82c004f47ee8ea9253942347e4392e
Abdur Rahman <abdur@lycus.com>
2023-08-25 18:18:45 +0500

    routes for login & session is created.

--- master/config/routes/api_v0.rb
+++ master/config/routes/api_v0.rb
@@ -1,5 +1,7 @@
 namespace :api do
   namespace :v0 do
+    post "/login", to: "authentication#login"
+    resource :session, only: :create
     resources :meshes do
       resources :slaves do
         resources :pins
