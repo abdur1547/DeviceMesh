b2c540a78d0e959c60441440409ae627181ac5a8
Abdur Rahman <abdur@lycus.com>
2023-08-25 18:17:17 +0500

    Application controller contains shared code for all API versions.

--- master/app/controllers/application_controller.rb
+++ master/app/controllers/application_controller.rb
@@ -1,51 +1,9 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user
-
-  rescue_from StandardError do |exception|
-    case exception.class.name
-    when ActiveRecord::RecordInvalid.name then unprocessable_entity(exception.message)
-    # when Pagy::VariableError.name then process_pagy_error(exception)
-    # when Pundit::NotAuthorizedError.name then unauthorized_response
-    else process_standard_error(exception)
-    end
+  def success_json_response(data:, serializer:, status:)
+    render json: {success: true, status: status, data: ActiveModelSerializers::SerializableResource.new(data, {serializer: serializer})}, status: status
   end
 
-  private
-
-  def authenticate_user
-    token = request.headers['Authorization']&.split(' ')&.last
-    decoded_token = decode_token(token)
-
-    if decoded_token
-      @current_user = User.find(decoded_token['user_id'])
-    else
-      render json: { error: 'Invalid token' }, status: :unauthorized
-    end
-  end
-
-  def decode_token(token)
-    JWT.decode(token, ENV['SECRET_KEY_BASE'])&.first
-  rescue JWT::DecodeError
-    nil
-  end
-
-  def current_user
-    @current_user
-  end
-
-  def process_standard_error(exception)
-    render json: { errors: [{ base: [exception.message] }] }, status: :internal_server_error
-  end
-
-  def unauthorized_response(reason = 'You are unauthorized to view this resource')
-    render json: { errors: [{ base: [reason] }] }, status: :unauthorized
-  end
-
-  def unprocessable_entity(error_message)
-    render json: { errors: [{ base: [error_message] }] }, status: :unprocessable_entity
-  end
-
-  def process_pagy_error(exception)
-    render json: { errors: [{ base: [exception.message] }] }, status: :unprocessable_entity
+  def failure_json_response(errors:, status:)
+    render json: {success: false, status: status, errors: errors}, status: status
   end
 end
