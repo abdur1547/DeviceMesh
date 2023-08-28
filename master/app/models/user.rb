4c5783caf8a0010922a88c1a18e39f76ae3f8afc
Abdur Rahman <abdur@lycus.com>
2023-08-28 17:14:44 +0500

    validation for user model is added.

--- master/app/models/user.rb
+++ master/app/models/user.rb
@@ -11,6 +11,12 @@
 #
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :full_name, presence: true
+  validates :password, presence: true
+  validates :password_digest, presence: true
   
   has_one :user_role, dependent: :destroy
   has_one :role, through: :user_role
