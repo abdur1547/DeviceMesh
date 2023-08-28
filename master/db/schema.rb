0519bb2b729db21a3e628a3f8d1693be80b75651
Abdur Rahman <abdur@lycus.com>
2023-08-28 17:46:55 +0500

    DB is migrated.

--- master/db/schema.rb
+++ master/db/schema.rb
@@ -61,11 +61,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_123059) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "full_name"
-    t.string "email"
+    t.string "full_name", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "meshes", "users"
