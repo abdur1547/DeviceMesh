37f71fbdefec12708cb82a541c8eb74f55a14641
Abdur Rahman <abdur@lycus.com>
2023-08-28 17:46:36 +0500

    user should have unique email.

--- master/db/migrate/20230717183919_create_users.rb
+++ master/db/migrate/20230717183919_create_users.rb
@@ -1,11 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :full_name
-      t.string :email
+      t.string :full_name, null: false, default: ""
+      t.string :email, null: false, default: ""
       t.string :password_digest
 
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
   end
 end
