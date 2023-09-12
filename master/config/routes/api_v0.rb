8179676b1ea3682091fed3b3a8f106487c8b6f80
Abdur Rahman <abdur@lycus.com>
2023-09-12 12:53:30 +0500

    signup route is added.

--- master/config/routes/api_v0.rb
+++ master/config/routes/api_v0.rb
@@ -1,6 +1,7 @@
 namespace :api do
   namespace :v0 do
     post "/login", to: "authentication#login"
+    post "/signup", to: "authentication#signup"
     resource :session, only: :create
     resources :meshes do
       resources :slaves do
