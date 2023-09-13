eaa6e286b1ddf3f64da83a2ec77c00992e151633
Abdur Rahman <abdur@lycus.com>
2023-09-13 14:25:38 +0500

    slaves create service is created.

---
+++ master/app/services/slave_service/create.rb
@@ -0,0 +1,21 @@
+module SlaveService
+  class Create < ApplicationService
+    attr_reader :name, :current_user
+    
+    def initialize(name, current_user)
+      @name = name
+      @current_user = current_user
+    end
+    
+    def call
+      mesh = Slave.create!(name: name, user_id: current_user.id)
+      return success(mesh)
+      rescue => e
+        return failure(e)
+    end
+  
+    def self.call(name:, current_user:)
+      new(name, current_user).call
+    end
+  end
+end
